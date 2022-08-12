3a2496a37e8eb7fe55cc5f5804314f2fc7bff8b9
Guilherme Correa <correacgui@gmail.com>
2022-08-12 12:18:02 -0300

    Only 'backdrop-filter: blur' on light mode

--- src/App.js
+++ src/App.js
@@ -28,12 +28,12 @@ const App = () => {
             </TooltipComponent>
           </div>
           {activeMenu ? (
-            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white overflow-hidden" style={{ borderRadius: '16px 0 0 16px', height: '85%' }}><Sidebar /></div>
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg backdrop-blur-[20em] overflow-hidden" style={{ borderRadius: '16px 0 0 16px', height: '85%' }}><Sidebar /></div>
           ) : (
             <div className="w-0 dark:bg-secondary-dark-bg"><Sidebar /></div>
           )}
-          <div className={`dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
-            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full"><Navbar /></div>
+          <div className={`backdrop-blur-[20em] min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
+            <div className="fixed md:static backdrop-blur-[20em] dark:bg-main-dark-bg navbar w-full"><Navbar /></div>
             <div>
               {themeSettings && <ThemeSettings />}
               <Routes>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,10 @@ module.exports = {
         'secondary-dark-bg': '#33373E',
         'light-gray': '#F7F7F7',
         'half-transparent': 'rgba(0, 0, 0, 0.5)',
+        'dark-main': '#1D203E',
+      },
+      backdropFilter: {
+        '20': '20em',
       },
       borderWidth: {
         1: '1px',
